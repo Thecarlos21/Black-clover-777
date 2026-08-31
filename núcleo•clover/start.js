c69269a5b9a07d759aefc59ad1df0cdbf612814d
AZAMIJS <thecuriositybot@gmail.com>
2026-08-30 23:09:54 -0600

    Zam

--- núcleo•clover/start.js
+++ núcleo•clover/start.js
@@ -23,7 +23,6 @@ import path, { join, dirname } from 'path'
 import { Boom } from '@hapi/boom'
 import { makeWASocket, protoType, serialize } from '../lib/simple.js'
 import { Low, JSONFile } from 'lowdb'
-import { mongoDB, mongoDBV2 } from '../lib/mongoDB.js'
 import store from '../lib/store.js'
 const { proto } = (await import('@whiskeysockets/baileys')).default
 import pkg from 'google-libphonenumber'
@@ -63,7 +62,7 @@ const __dirname = global.__dirname(import.meta.url)
 global.opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
 global.prefix = new RegExp('^[#/!.]')
 
-global.db = new Low(/https?:\/\//.test(opts['db'] || '')? new mongoDB(opts['db']) : new JSONFile('./src/database/database.json'))
+global.db = new Low(/https?:\/\//.test(opts['db'] || '')? new JSONFile('./src/database/database.json'))
 
 global.DATABASE = global.db
 global.loadDatabase = async function loadDatabase() {
